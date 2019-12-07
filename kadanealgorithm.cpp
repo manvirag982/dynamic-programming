85441ed557f9a7ec46f02088a3d8eed73e2ee039
manvirag982 <54881553+manvirag982@users.noreply.github.com>
2019-12-07 11:47:32 +0530

    Update kadanealgorithm.cpp

--- kadanealgorithm.cpp
+++ kadanealgorithm.cpp
@@ -1,4 +1,4 @@
-/*Given an array arr of N integers. Find the contiguous sub-array with maximum sum.
+/*Given an array arr of N integers. Find the contiguous sub-array with maximum sum.*/
 
 
 #include <bits/stdc++.h>
