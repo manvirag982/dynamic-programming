ce89ec71249fb9ed8012578c661e78f5fac8df0c
ANUBHAV GUPTA <anubhavguptadec1999@gmail.com>
2019-12-09 11:34:43 +0530

    Update formapalindrome.cpp

--- formapalindrome.cpp
+++ formapalindrome.cpp
@@ -115,6 +115,40 @@ int main()
 //dp tabulation  there are two method 1..find longest common subsequence of string and it reverse the subtract string length -lcs
 
 
+#include <bits/stdc++.h>
+using namespace std;
+int LCS(string s1 ,string s2){
+    
+    int dp[s1.size()+1][s2.size()+1];
+    
+    for(int i=0;i<=s1.size();i++)
+    {
+        for(int j=0;j<=s2.size();j++)
+        {
+            if(i==0||j==0)
+            dp[i][j]=0;
+            else if(s1[i-1]==s2[j-1])
+            dp[i][j]=dp[i-1][j-1]+1;
+            else
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            
+        }
+    }
+    return dp[s1.size()][s2.size()];
+}
+int main() 
+{
+	int t; cin>>t;
+	while(t--)
+	{
+	    string s2;  cin>>s2;
 
+	  string s1=s2;
+	  
+	  reverse(s2.begin(),s2.end());
+	    cout<<(s2.size()-(LCS(s1,s2)))<<endl;
+	}
+	return 0;
+}
 
 
